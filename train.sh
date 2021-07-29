bb29ffe6994a5c913f84bb39c0fd54e5f6bc1343
midpushgogo <790210592@qq.com>
2021-07-29 22:59:45 +0800

    change

--- train.sh
+++ train.sh
@@ -4,5 +4,5 @@ DATAPATH="data/"
 python main.py --dataset sceneflow \
     --datapath $DATAPATH --trainlist ./filenames/sceneflow_train.txt --testlist ./filenames/sceneflow_test.txt \
     --epochs 16 --lrepochs "10,12,14,16:2" \
-    --model gwcnet-gc --logdir ./checkpoints/sceneflow/gwcnet-gc \
-    --batch_size 4  --test_batch_size 4  --attention  \
+    --model gwcnet-gc --logdir ./checkpoints/sceneflow/gwcnet-noatt-gc \
+    --batch_size 4  --test_batch_size 4   \
